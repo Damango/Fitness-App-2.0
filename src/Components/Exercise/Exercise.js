57fc5c29cde2dda992e93a6855ffe636746d380d
Justin Kessler <redcoderhd1@gmail.com>
2021-06-08 22:11:15 -0400

    UI Changes

--- src/Components/Exercise/Exercise.js
+++ src/Components/Exercise/Exercise.js
@@ -3,11 +3,18 @@ import "./Exercise.css"
 
 const Exercise = (props) => {
 
-    {console.log(props)}
+
+    function addSet(){
+        
+    }
+
+
     return ( <div className="exercise-container">
        <div className="exercise-name">{props.data.name}</div>
+       <div className="categories-container">categories</div>
        <div className="sets-list-container">
            {props.data.sets.map((set) => <div className="set-block"></div>)}
+           <button className="add-set-button center-y">+</button>
        </div>
     </div> );
 }

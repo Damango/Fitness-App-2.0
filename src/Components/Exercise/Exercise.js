e9336a74ae06fee1c4c5388518d856d02b4ef7a5
Justin Kessler <redcoderhd1@gmail.com>
2021-06-12 21:54:51 -0400

    Setup for adding set

--- src/Components/Exercise/Exercise.js
+++ src/Components/Exercise/Exercise.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import "./Exercise.css"
+import axios from 'axios'
 
 const Exercise = (props) => {
 
 
     function addSet(){
+
+        let postObject;
+
+
+        axios.post(props.connection + '/user/addSet', postObject).then((res) => {
+            console.log(res)
+        })
 
         
     }
